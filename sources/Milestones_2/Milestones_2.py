bf7d36efa9058caccec222092738adbba1976853
JavierZM7 <113106041+JavierZM7@users.noreply.github.com>
2022-10-06 18:05:47 +0200

    Update Milestones_2.py

--- sources/Milestones_2/Milestones_2.py
+++ sources/Milestones_2/Milestones_2.py
@@ -1,12 +1,12 @@
 from Modulos.Cauchy_module import Cauchy
 from Modulos.Functions import Kepler
-from Modulos.Scheme_module import RK4,Euler,Inverse_Euler,Crank_Nicolson
+from Modulos.Scheme_module import RK4,Euler,Inverse_Euler,Crank_Nicolson #para usar newton, cambiar a Modulos.Scheme_module_Newton
 
 from numpy import array, linspace
 import matplotlib.pyplot as plt
 
 U0 = array([1.,0.,0.,1.])
-linspace_t = linspace(0,100,10001)                                          # linspace(i,f,d) el salto se calcula como (f-i)/(d-1) #N = linspace_t.shape[0]-1   # los pasos son las particiones de t -1
+linspace_t = linspace(0,100,10001)                                         
 
 Solución = Cauchy(Kepler,U0,linspace_t,Euler)
 #plot 
@@ -40,11 +40,8 @@ fig, ax = plt.subplots()
 ax.plot(Xorb,Yorb)
 plt.show()
 
-## si se aumentan las particiones en t, hasta los esquemas mas imprecisos se vuelven fiables
 # mismas condiciones iniciales
-linspace_t = linspace(0,100,100001) # linspace(i,f,d) el salto se calcula como (f-i)/(d-1)
-
-#N = linspace_t.shape[0]-1        #los pasos son las particiones de t -1
+linspace_t = linspace(0,100,100001) 
 
 Solución = Cauchy(Kepler,U0,linspace_t,Euler)
 #plot de la órbita
@@ -78,13 +75,8 @@ fig, ax = plt.subplots()
 ax.plot(Xorb,Yorb)
 plt.show()
 
-
-
-
 U0 = array([1.,0.,0.,1.])
-linspace_t = linspace(0,100,201) # linspace(i,f,d) el salto se calcula como (f-i)/(d-1)
-
-#N = linspace_t.shape[0]-1        #los pasos son las particiones de t -1
+linspace_t = linspace(0,100,201) 
 
 Solución = Cauchy(Kepler,U0,linspace_t,Crank_Nicolson)
 #plot de la órbita
